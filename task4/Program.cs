eb8f73e460314b04d8fea9cc089cd496dee331c1
Vladimir <volodya.kochetov.00@bk.ru>
2024-02-26 19:38:08 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую
+
+int n = 123;
+
+if (n < 10)
+{
+    Console.Write(n);
+}
+else
+{
+    while(n > 0)
+    {
+        int currentDigit = n % 10;
+        n /= 10;
+        if(n > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+}
