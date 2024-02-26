b730a26ff05f20dd1d82df69983c9cf16a6ff7a3
Vladimir <volodya.kochetov.00@bk.ru>
2024-02-26 18:56:35 +0300

    task3_victory

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int num = 12;
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+Console.WriteLine(maxDigit);
